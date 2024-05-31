cd5f50b92ff1f549b6218fb59e2388685fdd2824
Jeremiah Beber <jeremiah.beber@gmail.com>
2024-05-31 09:38:36 -0400

    Add initial functionality to iterate over a specified date range, auto generate folders, and transfer data files

--- main_vv.py
+++ main_vv.py
@@ -1,6 +1,16 @@
 import pysftp, shutil, os
 from secret import pwd
 from datetime import date, timedelta
+import calendar
+
+# Parse year, month, and day from string in YYYYMMDD format
+def parse_date(s):
+    year = int(s[:4])
+    month = int(s[4:6])
+    day = int(s[-2:])
+
+    return date(year, month, day)
+
 
 hostname = 's-9b0f88558b264dfda.server.transfer.us-east-1.amazonaws.com'
 export_id = '151717'
@@ -11,17 +21,33 @@ with pysftp.Connection(hostname,
                        private_key_pass=pwd) as sftp:
     
     sftp.chdir(export_id)
-    sftp.get('20240505/ItemSelectionDetails.csv', localpath='./ItemSelectionDetails_20240505.csv')
+    # sftp.get('20240505/ItemSelectionDetails.csv', 
+    #          localpath='./ItemSelectionDetails_20240505.csv')
 
-
-
-
-    # day_count = 21
-    # start_date = '20240506'
-    # seq = range(day_count)
-    # for single_date in (start_date + timedelta(n) for n in seq):
-    #     if seq % 7 == 0:
-    #         end_date = single_date
-    #         os.mkdir('')
+    day_count = 21
+    seq = range(day_count)
+    start_date = parse_date('20240506')
+    for single_date in (start_date + timedelta(n) for n in seq):
+        # If the current date is a Monday, create a 'week_ending' folder
+        # corresponding to the following Friday,
+        # then skip the Monday and add the rest of the data for the week
+        # to that folder
+        if calendar.day_name[single_date.weekday()] == 'Monday':
+            end_date = single_date + timedelta(6)
+            end_date_str = date.strftime(end_date, '%Y%m%d')
+            folder_name = f'Week_ending_{end_date_str}'
+            os.mkdir(folder_name)
+            os.chdir(folder_name)
+        elif calendar.day_name[single_date.weekday()] == 'Sunday':
+            date_str = date.strftime(single_date, '%Y%m%d')
+            sftp.get(f'{date_str}/ItemSelectionDetails.csv', 
+             localpath=f'./ItemSelectionDetails_{date_str}.csv')
+            os.chdir('../')   # After Sunday's data is collected, 
+                              # move back to the parent directory
+                              # to begin collection for the next week
+        else:
+            date_str = date.strftime(single_date, '%Y%m%d')
+            sftp.get(f'{date_str}/ItemSelectionDetails.csv', 
+             localpath=f'./ItemSelectionDetails_{date_str}.csv')
 
     
