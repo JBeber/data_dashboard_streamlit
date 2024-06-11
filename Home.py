4c8ca30140f7bc0b637e62706d9798957edfd8b4
Jeremiah Beber <jeremiah.beber@gmail.com>
2024-06-11 13:56:34 -0400

    move panini counts script to its own page

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import os
+
+# Main project directory
+main_dir = 'C:\\Users\\Jeremiah\\OneDrive\\Documents\\VV\\Daily_Order_Data\\data_dashboard_streamlit\\'
+
+os.chdir(main_dir)
+st.image('vv_logo.jpeg')
--- pages/Panini_Counts.py
+++ pages/Panini_Counts.py
@@ -3,7 +3,6 @@ import plotly.express as px
 from pandas import read_csv
 import os
 from datetime import date
-from calendar import day_name
 from VV_data_collect import parse_date
 
 # Main project directory
@@ -17,7 +16,7 @@ def get_directories():
     return lst
 
 data_directories = get_directories()
-print(data_directories)
+# print(data_directories)
 
 # List of strings to populate the selection box
 # display_weeks = [name.replace('_', ' ') for name in data_directories]
@@ -41,7 +40,7 @@ def load_data(filepath):
 
 # List of data filenames for the currently selected week
 week_data = [name for name in os.listdir('.')]
-print(week_data)
+# print(week_data)
 
 for filename in week_data:
     day_df = load_data(filename)
@@ -63,7 +62,7 @@ for filename in week_data:
 
     # Collect date from the current filename
     date_str = ''.join(filter(str.isdigit, filename))
-    print(date_str)
+    # print(date_str)
 
     # Date object for data currently being processed
     current_date = parse_date(date_str)
