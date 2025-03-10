af4e4c779fba6c76dd27a87595ce9a190a09fd04
JBeber <JBeber@users.noreply.github.com>
2025-03-10 17:30:47 +0000

    remove previous pages and archive in devel; add new page

--- Home.py
+++ Home.py
@@ -2,5 +2,5 @@ import streamlit as st
 import os
 # TODO: from VV_data_collect import collect_data
 
-# TODO: Add funxtionality to auto-detect when new data needs to be downloaded
+# TODO: Add functionality to auto-detect when new data needs to be downloaded
 st.image('vv_logo.jpeg')
