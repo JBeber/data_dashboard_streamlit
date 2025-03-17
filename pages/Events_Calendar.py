73237feb6785690bc1cf8554e6cda92059520ae4
JBeber <JBeber@users.noreply.github.com>
2025-03-17 16:05:50 +0000

    code Save Event functionality, add New Event class

--- pages/Events_Calendar.py
+++ pages/Events_Calendar.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 import streamlit as st
 from streamlit_calendar import calendar
 
@@ -10,6 +11,10 @@ st.markdown(
     "# VV Events Calendar"
 )
 
+with open('events.txt') as file:
+    contents = file.read()
+    calendar_events = contents.split("},")
+
 mode = st.selectbox(
     "Calendar Mode:",
     (
@@ -20,43 +25,57 @@ mode = st.selectbox(
     ),
 )
 
-def save_new_event(name, st_date, en_date, st_time, en_time):
+class NewEvent:
+    def __init__(self, name, st_dt, en_dt,
+                 allDay, st_tm, en_tm):
+        self.name = name
+        self.st_dt = st_dt
+        self.en_dt = en_dt
+        self.allDay = allDay
+        self.st_tm = st_tm
+        self.en_tm = en_tm
+
+
+
+def save_new_event(event):    
+    if event.st_tm is not None:
+        start = datetime.combine(event.st_dt, event.st_tm).isoformat(timespec='seconds')
+        end = datetime.combine(event.en_dt, event.en_tm).isoformat(timespec='seconds')
+    else:
+        start = event.st_dt.isoformat()
+        end = event.en_dt.isoformat()
+
+    calendar_events.append(
+        {
+            "title": event.name,
+            "color": "#FF6C6C",
+            "start": start,
+            "end": end,
+            "allDay": event.allDay,
+        },
+    )
     
-    if event_start_time is not None:
-        pass
+with st.popover('Add Event'):
+    event_name = st.text_input('Event Name')
 
-    
+    event_start_date = st.date_input("Start Date")
+    event_end_date = st.date_input("End Date")
 
-    # calendar_events.append(
-    #     {
-    #     "title": event.event_name,
-    #     "color": "#FF6C6C",
-    #     "start": ,
-    #     "end": ,
+    if event_start_date == event_end_date:
+        allday_event = st.checkbox('All day event?')
+    else:
+        allday_event = True
 
-    #     },
-    # )
-    
-new_event = st.popover('Add Event')
-event_name = new_event.text_input('Event Name')
+    if not allday_event:
+        event_start_time = st.time_input('Start Time')
+        event_end_time = st.time_input('End Time')
+    else:
+        event_start_time = event_end_time = None
 
-event_start_date = new_event.date_input("Start Date")
-event_end_date = new_event.date_input("End Date")
+    new_event = NewEvent(event_name, event_start_date, event_end_date,
+                         allday_event, event_start_time, event_end_time)
 
-if event_start_date == event_end_date:
-    allday_event = new_event.checkbox('All day event?')
-else:
-    allday_event = True
-
-if not allday_event:
-    event_start_time = new_event.time_input('Start Time')
-    event_end_time = new_event.time_input('End Time')
-else:
-    event_start_time = event_end_time = None
-
-new_event.button('Save', on_click=save_new_event, args=(event_name,event_start_date,event_end_date,
-                                                        event_start_time,event_end_time)
-)
+    st.button('Save', on_click=save_new_event, args=(new_event,))
 
 # Resources (e.g. specific rooms, equipment, or personnel
 # are not needed at this time, but can be specified later
@@ -112,39 +131,6 @@ elif mode == "List":
         "initialView": "listMonth",
     }
 
-calendar_events = [
-    {
-        "title": "Event 1",
-        "color": "#FF6C6C",
-        "start": "2025-03-10",
-        "end": "2025-03-13",
-    },
-    {
-        "title": "Event 2",
-        "color": "#FFBD45",
-        "start": "2025-03-01",
-        "end": "2025-03-03",
-    },
-    {
-        "title": "Event 3",
-        "color": "#FF4B4B",
-        "start": "2025-03-20",
-        "end": "2025-03-20",
-    },
-    {
-        "title": "Event 7",
-        "color": "#FF4B4B",
-        "start": "2025-03-20T08:30:00",
-        "end": "2025-03-20T10:30:00",
-    },
-    {
-        "title": "Event 17",
-        "color": "#FFBD45",
-        "start": "2025-03-13T15:30:00",
-        "end": "2025-03-13T16:30:00",
-    },
-]
-
 state = calendar(
     events=st.session_state.get("events", calendar_events),
     options=calendar_options,
@@ -168,7 +154,7 @@ state = calendar(
 if state.get("eventsSet") is not None:
     st.session_state["events"] = state["eventsSet"]
 
-# st.write(state)
+st.write(state)
 
 # st.markdown("## API reference")
 # st.help(calendar)
